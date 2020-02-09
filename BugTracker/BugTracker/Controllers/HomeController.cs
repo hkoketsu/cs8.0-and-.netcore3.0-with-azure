6653b7d77a9f445f2a9a84741a3d4082ab33cf40
hkoketsu <hiroki.koketsu@gmail.com>
2020-02-09 12:44:13 -0800

    Simple app using CosmoDB

---
+++ BugTracker/BugTracker/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BugTracker.Models;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using BugTracker.Data;
+using BugTracker.Interfaces;
+
+namespace BugTracker.Controllers
+{
+	public class HomeController : Controller
+	{
+		public readonly IWorkItemService _WorkItemService;
+
+		public HomeController(IWorkItemService workItemsService)
+		{
+			_WorkItemService = workItemsService;
+		}
+
+		public IActionResult Index()
+		{
+			var workItems = _WorkItemService.GetAllWorkItems();
+			return View(workItems);
+		}
+
+		public ActionResult AddWorkItem()
+		{
+			return RedirectToAction("AddItem", "AddWorkItem");
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
---
+++ BugTracker/BugTracker/Data/MongoDBRepository.cs
@@ -0,0 +1,30 @@
+﻿using BugTracker.Models;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BugTracker.Data
+{
+	public class MongoDBRepository
+	{
+		public readonly IMongoDatabase db;
+
+		public MongoDBRepository(IOptions<Settings> settings)
+		{
+			try
+			{
+				var mclient = new MongoClient(settings.Value.ConnectionString);
+				db = mclient.GetDatabase(settings.Value.Database);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("There was a problem connecting to the MongoDB database", ex);
+			}
+		}
+
+		public IMongoCollection<WorkItem> WorkItems => db.GetCollection<WorkItem>("workitem");
+	}
+}
